5dc09d09b1cacdbfcb3fd2fb75e8b39bf0535a16
Ollie Bennett <olliebennett@gmail.com>
2013-12-23 19:56:38 +0000

    Error logging in tweet_temperature.py

--- tweet_temperature.py
+++ tweet_temperature.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8
 
 import os
+import sys
 import olliebennett.logger as Log
 import olliebennett.twitter as Twitter
 
@@ -10,13 +11,17 @@ __author__ = 'Ollie Bennett | http://olliebennett.co.uk/'
 # Get current CPU temperature
 cmd = '/opt/vc/bin/vcgencmd measure_temp'
 line = os.popen(cmd).readline().strip()
-temp = line.split('=')[1].split("'")[0]
+try:
+    temp = line.split('=')[1].split("'")[0]
+except IndexError:
+    log_message = "Failed to determine CPU temperature using command '{0}'.".format(cmd)
+    Log.error(Log.TEMPERATURE, log_message)
+    Twitter.tweet("Error: " + log_message)
+    sys.exit()
 
-Log.debug(Log.TEMPERATURE, temp)
+Log.debug(Log.TEMPERATURE, "T=" + temp)
 
-status = 'My current CPU temperature is '+temp+'°C'
-
-Log.debug(Log.TWITTER, "New Status: %s" % status)
+status = 'My current CPU temperature is ' + temp + '°C'
 
 # Update status!
 Twitter.tweet(status)
