7fd13704984ddd0082d788cc95ee96ff9c176995
Ben Emery <ben.s.emery@gmail.com>
2013-12-26 22:50:21 +0000

    Series of small fixes

--- tweet_when_asked.py
+++ tweet_when_asked.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
-from olliebennett.steamers import TweetMyTemperature
+from olliebennett.config import twitter as TwitterConfig
+from olliebennett.streamers import TweetMyTemperature
 
 if __name__ == '__main__':
     # Set the thread running!
-    stream = TweetMyTemperature(handle=TwitterConfig.HANDLE, trigger='cpu_temp',
-                                 api.app_key, api.app_secret, api.oauth_token,
-                                 api.oauth_token_secret)
-    stream.statuses.filter(track=TwitterConfig.HANDLE)
+    stream = TweetMyTemperature(TwitterConfig.HANDLE, 'cpu_temp',
+                             TwitterConfig.CONSUMER_KEY, TwitterConfig.CONSUMER_SECRET,
+                             TwitterConfig.ACCESS_TOKEN, TwitterConfig.ACCESS_TOKEN_SECRET)
+    stream.statuses.filter(track='twitter')
