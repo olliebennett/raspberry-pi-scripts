e7ccfdf9d209f085fd5fac1af912f8d8ef8b8610
Ollie Bennett <olliebennett@gmail.com>
2013-12-28 15:23:36 +0000

    Only track/stream tweets mentioning our handle.

--- tweet_when_asked.py
+++ tweet_when_asked.py
@@ -6,6 +6,8 @@ from olliebennett.streamers import TweetMyTemperature
 if __name__ == '__main__':
     # Set the thread running!
     stream = TweetMyTemperature(TwitterConfig.HANDLE, 'cpu_temp',
-                             TwitterConfig.CONSUMER_KEY, TwitterConfig.CONSUMER_SECRET,
-                             TwitterConfig.ACCESS_TOKEN, TwitterConfig.ACCESS_TOKEN_SECRET)
-    stream.statuses.filter(track='twitter')
+                                TwitterConfig.CONSUMER_KEY, TwitterConfig.CONSUMER_SECRET,
+                                TwitterConfig.ACCESS_TOKEN, TwitterConfig.ACCESS_TOKEN_SECRET)
+
+    # We only want to receive tweets containing our twitter handle (i.e. mentions)
+    stream.statuses.filter(track=TwitterConfig.HANDLE)
