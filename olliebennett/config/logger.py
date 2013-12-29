5c95b5793d07ba9e7fc4e361798d7cbc527f5e08
Ollie Bennett <olliebennett@gmail.com>
2013-12-29 00:03:39 +0000

    Create new log for web server, and add favicon

--- olliebennett/config/logger.py
+++ olliebennett/config/logger.py
@@ -7,3 +7,5 @@ LOG_PATH_TWITTER = "logs" + os.sep + "twitter.log"
 LOG_PATH_TEMPERATURE = "logs" + os.sep + "temperature.log"
 
 LOG_PATH_TORRENT = "logs" + os.sep + "torrents.log"
+
+LOG_PATH_SERVER = "logs" + os.sep + "server.log"
--- olliebennett/logger.py
+++ olliebennett/logger.py
@@ -11,6 +11,7 @@ __author__ = 'Ollie Bennett | http://olliebennett.co.uk/'
 TWITTER = "Twitter"
 TEMPERATURE = "Temperature"
 TORRENT = "Torrent"
+SERVER = "Server"
 
 # Create loggers (for later use).
 formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-5s %(message)s', datefmt='%Y-%m-%d %I:%M:%S')
@@ -32,6 +33,13 @@ torrent_log.setLevel(logging.DEBUG)
 torrent_fh = logging.FileHandler(LogConfig.LOG_PATH_TORRENT)
 torrent_fh.setFormatter(formatter)  # add formatter to file handler
 torrent_log.addHandler(torrent_fh)  # add file handler to logger
+# Server
+server_log = logging.getLogger(SERVER)
+server_log.setLevel(logging.DEBUG)
+server_fh = logging.FileHandler(LogConfig.LOG_PATH_SERVER)
+server_fh.setFormatter(formatter)  # add formatter to file handler
+server_log.addHandler(server_fh)  # add file handler to logger
+
 
 
 def debug(log_type, log_message):
